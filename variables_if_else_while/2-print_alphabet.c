c42476ef85007c33f22cda7ab860681d6fa289f0
muniira-03 <muniira.alsubaie@gmail.com>
2025-02-16 08:04:57 +0000

    v2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,28 +1,37 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
     int n, last_digit;
+    char msg1[] = "Last digit of ";
+    char msg2[] = " is ";
+    char msg3[] = " and is greater than 5\n";
+    char msg4[] = " and is 0\n";
+    char msg5[] = " and is less than 6 and not 0\n";
+    char num_str[12];
 
     srand(time(0));
     n = rand() - RAND_MAX / 2;
-    last_digit = n % 10; /* استخراج آخر رقم */
+    last_digit = n % 10;
 
-    printf("Last digit of %d is %d", n, last_digit);
+    write(1, msg1, sizeof(msg1) - 1);
+    
+    sprintf(num_str, "%d", n);
+    write(1, num_str, sizeof(num_str) - 1);
+    
+    write(1, msg2, sizeof(msg2) - 1);
+
+    sprintf(num_str, "%d", last_digit);
+    write(1, num_str, sizeof(num_str) - 1);
 
     if (last_digit > 5)
-        printf(" and is greater than 5\n");
+        write(1, msg3, sizeof(msg3) - 1);
     else if (last_digit == 0)
-        printf(" and is 0\n");
+        write(1, msg4, sizeof(msg4) - 1);
     else
-        printf(" and is less than 6 and not 0\n");
+        write(1, msg5, sizeof(msg5) - 1);
 
     return (0);
 }
