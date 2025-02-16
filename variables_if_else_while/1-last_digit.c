2203d659eb114781cf51145ccec208ee5d8eb310
muniira-03 <muniira.alsubaie@gmail.com>
2025-02-16 08:12:50 +0000

    v1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,29 +1,27 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - Determines the last digit of a random number and prints a message
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int n, last_digit;
+	int n, last_digit;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    last_digit = n % 10; /* استخراج آخر رقم */
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
 
-    printf("Last digit of %d is %d", n, last_digit);
+	printf("Last digit of %d is %d ", n, last_digit);
 
-    if (last_digit > 5)
-        printf(" and is greater than 5\n");
-    else if (last_digit == 0)
-        printf(" and is 0\n");
-    else
-        printf(" and is less than 6 and not 0\n");
+	if (last_digit > 5)
+		printf("and is greater than 5\n");
+	else if (last_digit == 0)
+		printf("and is 0\n");
+	else
+		printf("and is less than 6 and not 0\n");
 
-    return (0);
+	return (0);
 }
-
