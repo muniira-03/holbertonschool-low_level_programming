259d115ceeaabf9440765bc502028a1100c3dc31
muniira-03 <muniira.alsubaie@gmail.com>
2025-03-04 11:00:48 +0000

    0-memset.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,28 +1,25 @@
 #include "main.h"
-#include <stddef.h>  /* Include this for NULL */
 
 /**
- * _strchr - Locates a character in a string.
- * @s: The string to be searched.
- * @c: The character to be located.
- *
- * Return: A pointer to the first occurrence of the character c,
- *         or NULL if the character is not found.
+ * _strchr - locates a character in a string.
+ * @s: string to search
+ * @c: character to find
+ * Return: pointer to the first occurrence of the character c in the string s,
+ *         or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
-    while (*s)  /* Loop through the string until the null-terminator */
-    {
-        if (*s == c)  /* If the current character matches the target */
-        {
-            return (s);  /* Return the pointer to the matching character */
-        }
-        s++;  /* Move to the next character */
-    }
-    if (c == '\0')  /* Check if the character is the null-terminator */
-    {
-        return (s);  /* Return pointer to the null-terminator */
-    }
-
-    return (NULL);  /* Character not found, return NULL */
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')  /* Check if c is the null terminator */
+	{
+		return (s);
+	}
+	return (NULL);
 }
