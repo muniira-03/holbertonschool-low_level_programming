8d9ddfe1ec10f3b3b928b5649fa28a2d3d65d095
muniira-03 <muniira.alsubaie@gmail.com>
2025-03-04 11:07:53 +0000

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -2,35 +2,29 @@
 
 /**
  * _strspn - gets the length of a prefix substring.
- * @s: The string to be scanned.
- * @accept: The string containing the characters to match.
- *
- * Return: The number of bytes in the initial segment of s
- *         which consist only of bytes from accept.
+ * @s: the string to search
+ * @accept: the characters to accept
+ * Return: the number of bytes in the initial segment of s which consist
+ *         only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
-    int i;
+	unsigned int count = 0;
+	char *ptr;
 
-    while (*s)  /* Loop through the string s */
-    {
-        /* Check if current character is in accept */
-        for (i = 0; accept[i] != '\0'; i++)
-        {
-            if (*s == accept[i])  /* Match found */
-            {
-                count++;
-                break;
-            }
-        }
-        
-        if (accept[i] == '\0')  /* If no match is found, break the loop */
-        {
-            break;
-        }
-        s++;
-    }
-
-    return (count);  /* Return the count of matching characters */
+	while (*s)
+	{
+		ptr = accept;
+		while (*ptr)
+		{
+			if (*s == *ptr)
+				break;
+			ptr++;
+		}
+		if (*ptr == '\0')
+			break;
+		count++;
+		s++;
+	}
+	return (count);
 }
