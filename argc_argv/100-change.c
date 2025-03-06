20f2181aa7db7c69d4dd5422780ca994263e4f63
muniira-03 <muniira.alsubaie@gmail.com>
2025-03-06 08:19:52 +0000

    100-change.c

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -2,29 +2,14 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * @argc: argument count
- * @argv: argument vector
+ * get_minimum_coins - calculates the minimum number of coins
+ * @cents: the amount of cents
  *
- * Return: 0 on success, 1 on error
+ * Return: the minimum number of coins
  */
-int main(int argc, char *argv[])
+int get_minimum_coins(int cents)
 {
-	int cents, coins = 0;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	cents = atoi(argv[1]);
-
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+	int coins = 0;
 
 	while (cents >= 25)
 	{
@@ -51,6 +36,36 @@ int main(int argc, char *argv[])
 		cents -= 1;
 		coins++;
 	}
+
+	return (coins);
+}
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	coins = get_minimum_coins(cents);
 
 	printf("%d\n", coins);
 	return (0);
