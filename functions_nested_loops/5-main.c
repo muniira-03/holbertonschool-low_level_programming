7690d3c53379accdcdc63c1e8fa9c53f6b9db3ad
muniira-03 <muniira.alsubaie@gmail.com>
2025-03-10 10:42:55 +0000

    5-sign.c

---
+++ functions_nested_loops/5-main.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - Entry point to test print_sign function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+
+	r = print_sign(0);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+
+	r = print_sign(0xff);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+
+	r = print_sign(-1);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+
+	return (0);
+}
