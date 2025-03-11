e9b209fd37910809c9282a16fbc0fe71a61dc679
muniira-03 <muniira.alsubaie@gmail.com>
2025-03-11 09:31:21 +0000

    100-times_table

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,28 +1,46 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_times_table - Prints the n times table, starting with 0
- * @n: The number for the times table
+ * print_number - Prints an integer using _putchar
+ * @n: Number to print
+ */
+void print_number(int n)
+{
+    if (n >= 10)
+        print_number(n / 10);
+    _putchar((n % 10) + '0');
+}
+
+/**
+ * print_times_table - Prints the n times table starting from 0
+ * @n: The times table to print
  */
 void print_times_table(int n)
 {
-	int i, j, product;
+    int i, j, result;
 
-	if (n < 0 || n > 15)
-		return;
+    if (n < 0 || n > 15)
+        return;
 
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			product = i * j;
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            result = i * j;
 
-			if (j == 0)
-				printf("%d", product);
-			else
-				printf(", %3d", product);
-		}
-		printf("\n");
-	}
+            if (j > 0) /* Print comma and spaces */
+            {
+                _putchar(',');
+                _putchar(' ');
+
+                /* Add proper spacing for alignment */
+                if (result < 10)
+                    _putchar(' '), _putchar(' ');
+                else if (result < 100)
+                    _putchar(' ');
+            }
+            print_number(result);
+        }
+        _putchar('\n');
+    }
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,6 +7,8 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
+void print_number(int n);
+void print_times_table(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 int _putchar(char c);
