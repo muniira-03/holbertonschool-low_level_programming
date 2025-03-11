731873d83d211b56fcd32d43f91ff3cbb79bae1a
muniira-03 <muniira.alsubaie@gmail.com>
2025-03-11 08:48:36 +0000

    101-natural

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - Prints the n times table, starting with 0
+ * @n: The number for the times table
+ */
+void print_times_table(int n)
+{
+	int i, j, product;
+
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			product = i * j;
+
+			if (j == 0)
+				printf("%d", product);
+			else
+				printf(", %3d", product);
+		}
+		printf("\n");
+	}
+}
