1a823d182283c0e5e245c24dad960ca355bb8b77
muniira-03 <muniira.alsubaie@gmail.com>
2025-03-11 08:44:10 +0000

    11-print_to_98.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,6 +5,7 @@ int print_last_digit(int n);
 void times_table(void);
 void times_table(void);
 int add(int a, int b);
+void print_to_98(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 int _putchar(char c);
