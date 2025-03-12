24886904cd2b0686f97708ff51c6b801888c6e6e
muniira-03 <muniira.alsubaie@gmail.com>
2025-03-12 08:58:18 +0000

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,27 +11,27 @@
  */
 char *_strdup(char *str)
 {
-    char *dup_str;
-    int i, len;
+	char *dup_str;
+	int i, len;
 
-    if (str == NULL)  /* If the input string is NULL, return NULL */
-        return (NULL);
+	if (str == NULL)  /* If the input string is NULL, return NULL */
+		return (NULL);
 
-    /* Calculate the length of the string */
-    for (len = 0; str[len] != '\0'; len++)
-        ;
+	/* Calculate the length of the string */
+	for (len = 0; str[len] != '\0'; len++)
+		;
 
-    /* Allocate memory for the duplicated string */
-    dup_str = malloc(sizeof(char) * (len + 1)); /* +1 for the null terminator */
-    if (dup_str == NULL)  /* If memory allocation fails, return NULL */
-        return (NULL);
+	/* Allocate memory for the duplicated string */
+	dup_str = malloc(sizeof(char) * (len + 1)); /* +1 for the null terminator */
+	if (dup_str == NULL)  /* If memory allocation fails, return NULL */
+		return (NULL);
 
-    /* Copy the contents of str to dup_str */
-    for (i = 0; i < len; i++)
-        dup_str[i] = str[i];
+	/* Copy the contents of str to dup_str */
+	for (i = 0; i < len; i++)
+		dup_str[i] = str[i];
 
-    /* Null-terminate the duplicated string */
-    dup_str[i] = '\0';
+	/* Null-terminate the duplicated string */
+	dup_str[i] = '\0';
 
-    return (dup_str);
+	return (dup_str);
 }
