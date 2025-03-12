da3ff2bd3bf20d95e8754c42c8417cc810341287
muniira-03 <muniira.alsubaie@gmail.com>
2025-03-12 17:51:34 +0000

    1-string_nconcat

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: number of bytes of s2 to concatenate.
+ *
+ * Return: pointer to newly allocated space with concatenated strings, or NULL
+ * if the function fails.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    unsigned int len1 = 0, len2 = 0, i, j;
+    char *concat;
+
+    /* Handle NULL as empty string*/
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+
+    /* Calculate the length of s1 */
+    while (s1[len1] != '\0')
+        len1++;
+
+    /* Calculate the length of s2*/
+    while (s2[len2] != '\0')
+        len2++;
+
+    /* If n is greater than or equal to length of s2, use the whole string*/
+    if (n >= len2)
+        n = len2;
+
+    /* Allocate memory for the concatenated string*/
+    concat = malloc(sizeof(char) * (len1 + n + 1)); /* +1 for null terminator*/
+    if (concat == NULL)
+        return (NULL);
+
+    /* Copy s1 into concat*/
+    for (i = 0; i < len1; i++)
+        concat[i] = s1[i];
+
+    /* Copy first n bytes of s2 into concat*/
+    for (j = 0; j < n; j++)
+        concat[i + j] = s2[j];
+
+    /* Null-terminate the concatenated string*/
+    concat[i + j] = '\0';
+
+    return (concat);
+}
