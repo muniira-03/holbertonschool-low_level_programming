91eb65c9a46256ef423e2889820b24922db24933
muniira-03 <muniira.alsubaie@gmail.com>
2025-03-13 09:08:00 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
@@ -8,40 +7,55 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- *
- * Return: pointer to the new dog or NULL if failure
+ * Return: pointer to the new dog, or NULL if it fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+    dog_t *new_dog;
 
-	/* Allocate memory for the new dog struct */
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-		return (NULL);
+    // Allocate memory for the new dog structure
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+        return (NULL);  // Return NULL if memory allocation fails
 
-	/* Allocate memory for the name and owner, and copy the strings */
-	new_dog->name = malloc(strlen(name) + 1);  /* +1 for the null terminator */
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);  /* Free the previously allocated memory for the dog struct */
-		return (NULL);
-	}
+    // Allocate memory and copy the name if it is not NULL
+    if (name != NULL)
+    {
+        new_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+        if (new_dog->name == NULL)
+        {
+            free(new_dog);  // Free previously allocated memory
+            return (NULL);
+        }
+        for (int i = 0; name[i] != '\0'; i++)
+            new_dog->name[i] = name[i];
+        new_dog->name[strlen(name)] = '\0';  // Null-terminate the string
+    }
+    else
+    {
+        new_dog->name = NULL;
+    }
 
-	new_dog->owner = malloc(strlen(owner) + 1);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);  /* Free the previously allocated memory for the name */
-		free(new_dog);  /* Free the dog struct */
-		return (NULL);
-	}
+    // Allocate memory and copy the owner if it is not NULL
+    if (owner != NULL)
+    {
+        new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+        if (new_dog->owner == NULL)
+        {
+            free(new_dog->name);  // Free previously allocated memory
+            free(new_dog);
+            return (NULL);
+        }
+        for (int i = 0; owner[i] != '\0'; i++)
+            new_dog->owner[i] = owner[i];
+        new_dog->owner[strlen(owner)] = '\0';  // Null-terminate the string
+    }
+    else
+    {
+        new_dog->owner = NULL;
+    }
 
-	/* Use strncpy to copy the strings safely */
-	strncpy(new_dog->name, name, strlen(name) + 1);  /*  nul terminator */
-	strncpy(new_dog->owner, owner, strlen(owner) + 1);  /* null terminator */
+    new_dog->age = age;
 
-	/* Set the dog's age */
-	new_dog->age = age;
-
-	return (new_dog);
+    return (new_dog);
 }
