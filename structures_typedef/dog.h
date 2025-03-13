92c74608f2cdd724ea6ebbf5743d7a19ec741478
muniira-03 <muniira.alsubaie@gmail.com>
2025-03-13 07:34:50 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,11 @@
+#ifndef DOG_H
+#define DOG_H
+
+/* Define a new type struct dog */
+struct dog {
+    char *name;   /* Dog's name */
+    float age;    /* Dog's age */
+    char *owner;  /* Dog's owner */
+};
+
+#endif /* DOG_H */
