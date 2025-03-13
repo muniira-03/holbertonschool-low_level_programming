2e80e0d43081465153546e31feed5ede37b0451c
muniira-03 <muniira.alsubaie@gmail.com>
2025-03-13 07:44:43 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,11 +2,17 @@
 #define DOG_H
 
 /* Define a new type struct dog */
+/**
+ * struct dog - A structure to represent a dog
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ */
 struct dog
 {
-    char *name;   /* Dog's name */
-    float age;    /* Dog's age */
-    char *owner;  /* Dog's owner */
+		char *name;   /* Dog's name */
+		float age;    /* Dog's age */
+		char *owner;  /* Dog's owner */
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
