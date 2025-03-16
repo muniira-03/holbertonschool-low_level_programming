d943cc70063266c84f9e06c00f126162656634b9
muniira-03 <muniira.alsubaie@gmail.com>
2025-03-16 08:38:19 +0000

    1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -8,11 +8,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i;
+	size_t i;
 
-    if (array == NULL || action == NULL)
-        return;
+	if (array == NULL || action == NULL)
+		return;
 
-    for (i = 0; i < size; i++)
-        action(array[i]);
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
