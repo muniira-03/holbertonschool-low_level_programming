964a27e6ca94b669cedb8278db1af776cb4e0188
muniira-03 <munirra.alsubaie@gmail.com>
2025-04-03 17:42:33 +0000

    1-dlistint_len.c

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -12,5 +12,6 @@ typedef struct dlistint_s
     struct dlistint_s *next;    /* Pointer to the next node */
 } dlistint_t;
 
+size_t dlistint_len(const dlistint_t *h);
 size_t print_dlistint(const dlistint_t *h);
 #endif
