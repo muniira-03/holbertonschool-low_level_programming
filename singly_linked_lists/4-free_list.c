3bc13a44cc2f82bee8bab3630a8c6192c3647bb4
muniira-03 <munirra.alsubaie@gmail.com>
2025-04-03 17:12:13 +0000

    4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t linked list
+ * @head: Pointer to the head of the list
+ */
+void free_list(list_t *head)
+{
+    list_t *temp;
+
+    while (head)
+    {
+        temp = head->next;  /* Store the next node */
+        free(head->str);     /* Free the duplicated string */
+        free(head);          /* Free the current node */
+        head = temp;         /* Move to the next node */
+    }
+}
