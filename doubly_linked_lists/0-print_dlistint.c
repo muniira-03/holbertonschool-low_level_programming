3480da0e1956a7b7a7e89b1b377bdce03fa51bea
muniira-03 <munirra.alsubaie@gmail.com>
2025-04-05 13:37:54 +0000

    0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,19 +3,19 @@
 /**
  * print_dlistint - Prints all the elements of a doubly linked list
  * @h: A pointer to the head of the doubly linked list
- * 
+ *
  * Return: The number of nodes in the list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;
+    size_t count = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);	/* Print the value of the current node */
-		count++;			/* Increment the node counter */
-		h = h->next;		/* Move to the next node in the list */
-	}
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        count++;
+        h = h->next;
+    }
 
-	return (count);		/* Return the total number of nodes */
+    return (count);
 }
