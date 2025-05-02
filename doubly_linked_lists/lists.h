ca91c8d649bb4be55ff860dfc43d407daeaacdf2
muniira-03 <munirra.alsubaie@gmail.com>
2025-05-02 19:20:01 +0000

    0

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -3,22 +3,29 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-/* Structure for doubly linked list */
-typedef struct dlistint_s
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
 {
-    int n;                     /* Integer data of the node */
-    struct dlistint_s *prev;    /* Pointer to the previous node */
-    struct dlistint_s *next;    /* Pointer to the next node */
-} dlistint_t;
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
 
-size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-size_t print_dlistint(const dlistint_t *h);
-void free_dlistint(dlistint_t *head);
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
-int sum_dlistint(dlistint_t *head);
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+
+
 #endif
