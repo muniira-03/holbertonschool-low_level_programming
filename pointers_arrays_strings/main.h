f099eb7a7959fc266de888cf7586fd05db60a857
muniira-03 <munirra.alsubaie@gmail.com>
2025-05-02 17:23:00 +0000

    main header

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,23 @@
-char *_strpbrk(char *s, char *accept);
-char *_strchr(char *s, char c);
-void set_string(char **s, char *to);
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+* void prototypes()
+*char prototypes()
+* int prototypes()
+*/
+void reset_to_98(int *n);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void swap_int(int *a, int *b);
+void swap_char(char *a, char *b);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+int _strlen(char *s);
+int _atoi(char *s);
+#endif /*MAIN_H*/
