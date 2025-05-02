ad0827253b194db51e2217146cf25643c14c6620
muniira-03 <munirra.alsubaie@gmail.com>
2025-05-02 19:07:39 +0000

    header file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,23 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-
 /**
-* void prototypes()
-*char prototypes()
-* int prototypes()
-*/
-void reset_to_98(int *n);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void swap_int(int *a, int *b);
-void swap_char(char *a, char *b);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _putchar(char c);
-int _strlen(char *s);
-int _atoi(char *s);
+ *void prototypes()
+ *int prototypes()
+ *char prototypes()
+ */
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, int size);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+
 #endif /*MAIN_H*/
